a6fb5d6d0e690b28c495f5363174853921b6d592
Tornado <95181993+Tornado046@users.noreply.github.com>
2025-09-18 21:54:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,252 @@
+{
+  "result": true,
+  "owner": "0xf9018f30198F0131bbF1dDBcACf743BBE173F968",
+  "contribution": [
+    {
+      "kingdomId": "614754a50d6be81261240f71",
+      "total": 800,
+      "name": "🌪🇨🇱1★opɐuɹoʇ",
+      "continent": 2
+    },
+    {
+      "kingdomId": "62f99ffa8daaa506af24661a",
+      "total": 503.625,
+      "name": "ALE)(I",
+      "continent": 60
+    },
+    {
+      "kingdomId": "644fcff603d6734c61347d9a",
+      "total": 422.325,
+      "name": "ALE)(II",
+      "continent": 25
+    },
+    {
+      "kingdomId": "610ef420cfa88f64b14743fc",
+      "total": 176,
+      "name": "Tornado_Land",
+      "continent": 2
+    },
+    {
+      "kingdomId": "61eae7a5e3e20910dce71d03",
+      "total": 120,
+      "name": "TheNecro TR ◆",
+      "continent": 25
+    },
+    {
+      "kingdomId": "61f1e7f005d34d23bc161426",
+      "total": 120,
+      "name": "Firulais⁴",
+      "continent": 59
+    },
+    {
+      "kingdomId": "62d56f4eb110636f10e3da38",
+      "total": 120,
+      "name": "OneaLᴰᴿc",
+      "continent": 17
+    },
+    {
+      "kingdomId": "6261775665bf476d13b7da7f",
+      "total": 120,
+      "name": "OMforever-2",
+      "continent": 17
+    },
+    {
+      "kingdomId": "61b0b1f30d36b20e0169f73b",
+      "total": 40,
+      "name": "Mty10",
+      "continent": 59
+    },
+    {
+      "kingdomId": "61e9089a9d65221532083240",
+      "total": 40,
+      "name": "beaver.",
+      "continent": 24
+    },
+    {
+      "kingdomId": "61dddb6b48d7a243a27f7da7",
+      "total": 40,
+      "name": "UBTokmak",
+      "continent": 2
+    },
+    {
+      "kingdomId": "655b49ea55a3ed3eaa765c35",
+      "total": 32.5,
+      "name": "Xgana",
+      "continent": 59
+    },
+    {
+      "kingdomId": "615ec37252db5a4d5a7ab619",
+      "total": 24,
+      "name": "persoan",
+      "continent": 20
+    },
+    {
+      "kingdomId": "654fbb43d0ca8e69e408f06e",
+      "total": 20,
+      "name": "Sky Fox",
+      "continent": 19
+    },
+    {
+      "kingdomId": "60e5c3b90c24c66b7c2bd8a2",
+      "total": 20,
+      "name": "leongƱ",
+      "continent": 25
+    },
+    {
+      "kingdomId": "61c407e6743cb35bf9d368bf",
+      "total": 20,
+      "name": "kings 2022",
+      "continent": 17
+    },
+    {
+      "kingdomId": "6196d6e4dd36d20c4e8656b6",
+      "total": 20,
+      "name": "NeomRev▪︎TR¥",
+      "continent": 24
+    },
+    {
+      "kingdomId": "60ee9b23ad656f647516f2c7",
+      "total": 18,
+      "name": "Seris ♥︎",
+      "continent": 60
+    },
+    {
+      "kingdomId": "63a2c8b3427d5a368ed457d2",
+      "total": 12,
+      "name": "BìnhNguyênStone",
+      "continent": 2
+    },
+    {
+      "kingdomId": "64182249a86e496bd6ed452a",
+      "total": 12,
+      "name": "*\" KAMI \"*",
+      "continent": 17
+    },
+    {
+      "kingdomId": "65b01d1a87f2fd4d21fad923",
+      "total": 12,
+      "name": "Ketteyosss",
+      "continent": 59
+    },
+    {
+      "kingdomId": "61ddb1765840fb481447599a",
+      "total": 12,
+      "name": "Godbless",
+      "continent": 17
+    },
+    {
+      "kingdomId": "651ed58128636b32ae9da21e",
+      "total": 12,
+      "name": "Naklua3",
+      "continent": 60
+    },
+    {
+      "kingdomId": "63fb51bb4375d74552bb3692",
+      "total": 12,
+      "name": "Tiểu Công Tử の",
+      "continent": 2
+    },
+    {
+      "kingdomId": "61d743829e71b31171204ab1",
+      "total": 12,
+      "name": "*♤DayiLanD♤*",
+      "continent": 24
+    },
+    {
+      "kingdomId": "659d293e3061f819a6ab293e",
+      "total": 12,
+      "name": "Ksafius",
+      "continent": 59
+    },
+    {
+      "kingdomId": "613b4cc1b467ff5b7c497c01",
+      "total": 12,
+      "name": "JefsWorld_1",
+      "continent": 2
+    },
+    {
+      "kingdomId": "6247e326e479226cde8673f1",
+      "total": 12,
+      "name": "Mr - Quân W",
+      "continent": 60
+    },
+    {
+      "kingdomId": "64eb7a4dd5512b4485f15d9e",
+      "total": 6.25,
+      "name": "2Hio0w",
+      "continent": 60
+    },
+    {
+      "kingdomId": "61a7c42c2397fa0c3a54a6f0",
+      "total": 6,
+      "name": "Instinto SP",
+      "continent": 25
+    },
+    {
+      "kingdomId": "61b2e9e2c693ef0c5db6267b",
+      "total": 6,
+      "name": "snowstorm",
+      "continent": 24
+    },
+    {
+      "kingdomId": "60f30210719bfc04ac809895",
+      "total": 6,
+      "name": "Mingming Cat",
+      "continent": 60
+    },
+    {
+      "kingdomId": "619f2aa9e2f8740c7d3f11f8",
+      "total": 6,
+      "name": "酷似一阵风",
+      "continent": 20
+    },
+    {
+      "kingdomId": "64e9bc2871b21b573ff31502",
+      "total": 2.5,
+      "name": "With0t3",
+      "continent": 60
+    },
+    {
+      "kingdomId": "61a43f57e620810c50bd4d96",
+      "total": 2.07788,
+      "name": "_.萢燒",
+      "continent": 19
+    },
+    {
+      "kingdomId": "64e9c6bae880a614e5fc840e",
+      "total": 1.25,
+      "name": "Davi08y",
+      "continent": 60
+    },
+    {
+      "kingdomId": "64eb7aba1c24575e25ed6d43",
+      "total": 1.25,
+      "name": "S21samsung",
+      "continent": 60
+    },
+    {
+      "kingdomId": "654327d216c65860a86b21fa",
+      "total": 1.08996,
+      "name": "chiikawa⑥",
+      "continent": 17
+    },
+    {
+      "kingdomId": "64e9b8f0cff5915db33b2e13",
+      "total": 0.175,
+      "name": "Dog0d2t",
+      "continent": 60
+    },
+    {
+      "kingdomId": "628d0b67ac62996dc5949448",
+      "total": 0.1,
+      "name": "i am 4",
+      "continent": 17
+    },
+    {
+      "kingdomId": "64eb75eab5a0dc4b546a3744",
+      "total": 0.05,
+      "name": "Piro1Y3",
+      "continent": 60
+    }
+  ]
+}
